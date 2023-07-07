4efffea432b301e1e79da9fdd3e35043f50fccf9
Jon Mico <jonmico@gmail.com>
2023-07-07 12:40:24 -0400

    Add some more styling/text.

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,58 @@ const StyledCard = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 85%;
+  width: 320px;
+  min-width: 320px;
+  background-color: hsl(0, 0%, 100%);
+  border-radius: 10px;
+`;
+
+const StyledPHeader = styled.p`
+  text-transform: uppercase;
+  color: hsl(228, 12%, 48%);
+  font-size: 0.75rem;
+  letter-spacing: 6px;
+`;
+
+const StyledContentWrapper = styled.div`
+  padding: 1.25rem;
+  width: 100%;
+`;
+
+const StyledH1 = styled.h1`
+  font-family: Fraunces;
+  font-size: 1.75rem;
+  margin-top: 0.75rem;
+  line-height: 1;
+  color: hsl(212, 21%, 14%);
+`;
+
+const StyledP = styled.p`
+  color: hsl(228, 12%, 48%);
+  font-size: 0.85rem;
+  margin-top: 0.75rem;
+  line-height: 1.5;
+`;
+
+const StyledPriceDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-top: 1rem;
+`;
+
+const BigPriceSpan = styled.span`
+  color: hsl(158, 36%, 37%);
+  font-family: Fraunces;
+  font-size: 1.75rem;
+`;
+
+const SmallPriceSpan = styled.span`
+  color: hsl(228, 12%, 48%);
+  text-decoration: line-through;
+  font-size: 0.75rem;
+  margin-left: 1.25rem;
 `;
 
 export default function App() {
@@ -23,6 +74,18 @@ export default function App() {
     <StyledMainWrapper>
       <StyledCard>
         <Image />
+        <StyledContentWrapper>
+          <StyledPHeader>Perfume</StyledPHeader>
+          <StyledH1>Gabrielle Essence Eau De Parfum</StyledH1>
+          <StyledP>
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </StyledP>
+          <StyledPriceDiv>
+            <BigPriceSpan>$149.99</BigPriceSpan>
+            <SmallPriceSpan>$169.99</SmallPriceSpan>
+          </StyledPriceDiv>
+        </StyledContentWrapper>
       </StyledCard>
     </StyledMainWrapper>
   );
